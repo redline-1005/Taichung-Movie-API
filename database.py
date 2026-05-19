6ffeb8409066d179d746b9f66400f02d2624df7b
redline-1005 <05justsayhi@gmail.com>
2026-05-19 10:56:58 +0800

    修正database絕對路徑

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 # database.py
+import os
 from sqlmodel import create_engine, SQLModel, Session
 from models import Theater, Showtime
 
+# 1. 動態獲取當前 database.py 所在的資料夾絕對路徑
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# 2. 確保檔名為 movie_database.db
 sqlite_file_name = "movie_database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+sqlite_url = f"sqlite:///{os.path.join(BASE_DIR, sqlite_file_name)}"
 
-engine = create_engine(sqlite_url, echo=False)
+# connect_args 是為了防止 SQLite 在多執行緒（FastAPI）下塞車
+engine = create_engine(sqlite_url, echo=False, connect_args={"check_same_thread": False})
 
 def init_db():
     """初始化資料庫並建立表格"""
